6c9afca4e5c722153548872d9812f70604a99626
Henrique <henriquelira99@gmail.com>
2019-11-05 21:56:37 -0300

    Add appointment listing

--- src/database/migrations/20191105011115-create-appointments.js
+++ src/database/migrations/20191105011115-create-appointments.js
@@ -25,7 +25,7 @@ module.exports = {
         onDelete: 'SET NULL',
         allowNull: false,
       },
-      cancelled_at: {
+      canceled_at: {
         type: Sequelize.DATE,
       },
       created_at: {
